56265e1822204b44eb25da0365726e3681139d5e
Sebastian Stubenrauch <sebastian.stubenrauch@gmx.at>
2020-11-23 20:16:18 +0100

    Sebastian
    projectile based shooting

---
+++ Assets/Scripts/bulletScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float expiryTime = 0f;
+
+ 
+    void Start()
+    {
+        Destroy(gameObject, expiryTime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player") Destroy(gameObject);
+    }
+}
