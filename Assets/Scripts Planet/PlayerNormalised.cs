6182c0e26221fdb70c7011eb135318ed9d02b14d
Sebastian Stubenrauch <sebastian.stubenrauch@gmx.at>
2020-12-08 13:58:59 +0100

    Sebastian
    Sample Scene

---
+++ Assets/Scripts Planet/PlayerNormalised.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNormalised : MonoBehaviour
+{
+
+    public void Attract(Transform body)
+    {
+        Vector3 targetDir = (body.position - transform.position).normalized;
+        Vector3 bodyUp = body.up;
+
+        body.rotation = Quaternion.FromToRotation(bodyUp, targetDir) * body.rotation;
+    }
+}
---
+++ Assets/Scripts Planet/PlayerNormalised2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerNormalised2 : MonoBehaviour
+{
+
+    PlayerNormalised planetcore;
+
+    void Awake()
+    {
+        planetcore = GameObject.FindGameObjectWithTag("PlanetCore").GetComponent<PlayerNormalised>();
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    void FixedUpdate()
+    {
+        planetcore.Attract(transform);
+    }
+}
